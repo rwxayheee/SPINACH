5cd16616588feaac0aca8cec5b48c7d6075cc4ea
Amy He <87205830+rwxayheee@users.noreply.github.com>
2023-05-03 14:40:59 -0400

    Update mystats.py
    now has the utils

--- mystats.py
+++ mystats.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # Variance of Shifted Data to avoid the catastrophic cancellation
 def svar(mydata):
     # https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
@@ -36,4 +38,44 @@ def accor(mydata,lag_max=200):
         myacorr[l] = c
         
     return myacorr
+
+# Accumulated Average
+def accavg(mydata):
+    N = len(mydata)
+    
+    myacc=[]
+    for i in range(1,N+1):
+        myacc.append(sum(mydata[:i])/i)
+    
+    return myacc
+
+# Utils
+
+def en_parser(en_file):
+    with open(en_file,"r") as f:
+        line = f.readline()
+        mydvdl = []
+        while line:
+            if 'L9' in line:
+                mydvdl.append(line.split()[-1])
+            line = f.readline()
+    mydvdl = [float(x) for x in mydvdl[1:]]
+    
+    return mydvdl
+
+def dvdl_stats(dvdldat):
+    avgserial = accavg(dvdldat)
+    return({"End-Point Average":avgserial[-1], "End-Point Variance":svar(avgserial)})
+
+def make_integrand(Xs, en_list):
+    fs = []
+    vs = []
+    for en in en_list:
+        en_dvdl = en_parser(en)
+        fs.append(dvdl_stats(en_dvdl)["End-Point Average"])
+        vs.append(dvdl_stats(en_dvdl)["End-Point Variance"])
+    return({"Xs":Xs, "fs":fs, "vs": vs})
+
+def propvar(cubo, vs):
+    return(sum(np.matmul(cubo.W_extra,np.array(vs).T)))
 
