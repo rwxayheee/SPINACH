32f9081f9afca93793d476a3f596db9678f9f22c
Amy He <87205830+rwxayheee@users.noreply.github.com>
2023-05-05 09:31:42 -0400

    Update interpolators.py
    adds linear interpolator

--- interpolators.py
+++ interpolators.py
@@ -117,3 +117,70 @@ class Cubic:
             if Extend in ["Linear","Natural"]:
                 self.Imin = sum(np.matmul(self.Wmin,np.array(fs).T))
                 self.Imax = sum(np.matmul(self.Wmax,np.array(fs).T))
+                
+class Linear:
+
+    # constructor function
+    def __init__(self, Xs = [], fs = [], Integrate = True, Extend = "Linear", Xmin = None, Xmax = None):
+        self.Xs = Xs
+        self.fs = fs
+        self.dimension = len(Xs)
+        
+        # 0. Computes H
+        self.H = [self.Xs[i]-self.Xs[i-1] for i in range(1,self.dimension)]
+
+        # 1. Populates A
+        AinF = np.zeros((self.dimension, self.dimension))
+        np.fill_diagonal(AinF, 1)
+        self.AinF = AinF
+
+        # 2. Solves B
+        BinF = [[0 for _ in range(self.dimension)] for _ in range(self.dimension)]
+        for i in range(1,self.dimension):
+            BinF[i-1] += (self.AinF[i]-self.AinF[i-1])/self.H[i-1]
+        BinF[-1] = np.matrix(BinF[-1])
+        self.BinF = BinF
+
+        # 3. Weight matrix of F for an integral of the interpolated segments
+        W_inter = []
+        for i in range(self.dimension-1):
+            Wi = np.matrix(self.H[i]*self.AinF[i]\
+                           +np.power(self.H[i],2)*self.BinF[i]/2)
+            W_inter.append(np.matrix(Wi))
+
+        # 4. Extrapolates and Updates Weights
+        self.W_extra = W_inter
+        if Extend in ["Linear"]:
+            
+            hmin = Xs[0]-Xmin
+            
+            Amin = hmin*AinF[0]
+            self.AinF[0] += Amin
+            Bmin = -np.power(hmin,2)*BinF[0]/2
+            self.BinF[0] += Bmin
+            
+            self.Wmin = np.matrix(Amin + Bmin)
+            
+            hmax = Xmax-Xs[-1]
+            
+            Amax = hmax*AinF[-2]
+            self.AinF[-2] += Amax
+            Bmax = (np.power(hmax+self.H[-1],2)-np.power(self.H[-1],2))*BinF[-2]/2
+            self.BinF[-2] += Bmax
+            
+            self.Wmax = np.matrix(Amax + Bmax)
+
+            W_extra = [self.Wmin]
+            for i in range(0,self.dimension-1):
+                W_extra.append(self.W_extra[i])
+            W_extra.append(self.Wmax)
+            self.W_extra = W_extra
+        
+        # 5. Integrates in segments
+        if Integrate == True:
+            self.I_seg = np.matmul(self.W_extra,np.array(fs).T)
+            self.I = sum(self.I_seg)
+            self.Iint = sum(np.matmul(W_inter,np.array(fs).T))
+            if Extend in ["Linear"]:
+                self.Imin = sum(np.matmul(self.Wmin,np.array(fs).T))
+                self.Imax = sum(np.matmul(self.Wmax,np.array(fs).T))
